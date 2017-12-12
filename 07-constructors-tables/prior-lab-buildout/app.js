19f2ceb15bd2eddd56f6d36e61bfc79a99322e95
Sam Hamm <sam@codefellows.com>
2017-12-12 10:20:16 -0800

    first break

--- 07-constructors-tables/prior-lab-buildout/app.js
+++ 07-constructors-tables/prior-lab-buildout/app.js
@@ -6,3 +6,37 @@
 // Seattle Center 11	38	3.7
 // Capitol Hill	  20	38	2.3
 // Alki	          2	  16	4.6
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; // via MDN docs
+}
+
+var pikePlaceMarket = {
+  name: '1st and Pike',
+  minCustsPerHour: 23,
+  maxCustsPerHour: 65,
+  avgCookiesPerCust: 6.3,
+  custsEachHour: [],
+  cookiesEachHour: [],
+  totalDailySales: 0,
+  calcCustsEachHour: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+    }
+  },
+  calcCookiesEachHour: function(){
+    this.calcCustsEachHour();
+    for(var i = 0; i < hours.length; i++){
+      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
+      console.log(oneHour, 'one hour');
+      this.cookiesEachHour.push(oneHour);
+      this.totalDailySales += oneHour;
+      console.log(this.totalDailySales, 'total');
+    }
+  },
+  render: function(){
+
+  }
+}
