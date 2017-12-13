2e985012d9e5e0d6c1c0e8a0b4afb47447eada35
Sam Hamm <samhamm@gmail.com>
2017-12-13 12:26:49 -0800

    end of class 12/13

--- 07-constructors-tables/prior-lab-buildout/app.js
+++ 07-constructors-tables/prior-lab-buildout/app.js
@@ -1,109 +1,109 @@
 'use strict';
 
-// Location	      Min Max Avg
-// 1st and Pike	  23	65	6.3
-// SeaTac Airport 3	  24	1.2
-// Seattle Center 11	38	3.7
-// Capitol Hill	  20	38	2.3
-// Alki	          2	  16	4.6
+// +++++++++++++++++++++++++++++++++++++++
+// DATA
+// +++++++++++++++++++++++++++++++++++++++
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+var salestable = document.getElementById('salestable');
+
+var allLocations = [];
+
+function CookieStand(name, min, max, avg) {
+  this.name = name;
+  this.minCustsPerHour = min;
+  this.maxCustsPerHour = max;
+  this.avgCookiesPerCust = avg;
+  this.custsEachHour = [];
+  this.cookiesEachHour = [];
+  this.totalDailySales = 0;
+  allLocations.push(this);
+}
+
+new CookieStand('1st and Pike', 23, 65, 6.3);
+new CookieStand('SeaTac Airport', 3, 24, 1.2);
+new CookieStand('Seattle Center', 11, 38, 3.7);
+new CookieStand('Capitol Hill', 20, 38, 2.3);
+new CookieStand('Alki', 2, 16, 4.6);
+
+// +++++++++++++++++++++++++++++++++++++++
+// DEFINE ACTIONS (DECLARE FUNCTIONS)
+// +++++++++++++++++++++++++++++++++++++++
+
+CookieStand.prototype.calcCustsEachHour = function(){
+  for(var i = 0; i < hours.length; i++){
+    this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+  }
+};
+
+CookieStand.prototype.calcCookiesEachHour = function(){
+  this.calcCustsEachHour();
+  for(var i = 0; i < hours.length; i++){
+    var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
+    // console.log(oneHour, 'one hour');
+    this.cookiesEachHour.push(oneHour);
+    this.totalDailySales += oneHour;
+    // console.log(this.totalDailySales, 'total');
+  }
+};
+
+CookieStand.prototype.render = function(){
+  this.calcCookiesEachHour();
+  // create a tr
+  var trEl = document.createElement('tr');
+  // create td, content, append for the store name
+  var thEl = document.createElement('th');
+  thEl.textContent = this.name;
+  trEl.appendChild(thEl);
+  // create tds, give them content, append for hourly numbers
+  for(var i = 0; i < hours.length; i++) {
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.cookiesEachHour[i];
+    trEl.appendChild(tdEl);
+  }
+  // create td, give it content, append for the daily totalDailySales
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.totalDailySales;
+  trEl.appendChild(tdEl);
+  // append the tr to the table
+  salestable.appendChild(trEl);
+};
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; // via MDN docs
 }
 
-var pikePlaceMarket = {
-  name: '1st and Pike',
-  minCustsPerHour: 23,
-  maxCustsPerHour: 65,
-  avgCookiesPerCust: 6.3,
-  custsEachHour: [],
-  cookiesEachHour: [],
-  totalDailySales: 0,
-  calcCustsEachHour: function(){
-    for(var i = 0; i < hours.length; i++){
-      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
-    }
-  },
-  calcCookiesEachHour: function(){
-    this.calcCustsEachHour();
-    for(var i = 0; i < hours.length; i++){
-      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
-      // console.log(oneHour, 'one hour');
-      this.cookiesEachHour.push(oneHour);
-      this.totalDailySales += oneHour;
-      // console.log(this.totalDailySales, 'total');
-    }
-  },
-  render: function(){
-    this.calcCookiesEachHour();
-    // access the element in the DOM where our stuff will go
-    var ulEl = document.getElementById('pike');
-    // console.log(ulEl, 'ulEl');
-    for(var i = 0; i < hours.length; i++){
+function makeHeaderRow() {
+  // create a tr
+  var trEl = document.createElement('tr');
 
-      // create an element
-      var liEl = document.createElement('li');
+  // create td, content, append for the store name
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Location Name';
+  trEl.appendChild(thEl);
 
-      // give it content
-      // 11am: 77 cookies
-      liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies';
-
-      // append it to the parent
-      ulEl.appendChild(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = 'Total: ' + this.totalDailySales + ' cookies';
-    ulEl.appendChild(liEl);
+  // create tds, give them content, append for hourly numbers
+  for(var i = 0; i < hours.length; i++) {
+    thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
   }
+
+  // create th, give it content, append for the daily totalDailySales
+  thEl = document.createElement('th');
+  thEl.textContent = 'Totals';
+  trEl.appendChild(thEl);
+
+  // append the tr to the table
+  salestable.appendChild(trEl);
 }
 
-var seatacAirport = {
-  name: 'Seatac Airport',
-  minCustsPerHour: 3,
-  maxCustsPerHour: 24,
-  avgCookiesPerCust: 1.2,
-  custsEachHour: [],
-  cookiesEachHour: [],
-  totalDailySales: 0,
-  calcCustsEachHour: function(){
-    for(var i = 0; i < hours.length; i++){
-      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
-    }
-  },
-  calcCookiesEachHour: function(){
-    this.calcCustsEachHour();
-    for(var i = 0; i < hours.length; i++){
-      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
-      // console.log(oneHour, 'one hour');
-      this.cookiesEachHour.push(oneHour);
-      this.totalDailySales += oneHour;
-      // console.log(this.totalDailySales, 'total');
-    }
-  },
-  render: function(){
-    this.calcCookiesEachHour();
-    // access the element in the DOM where our stuff will go
-    var ulEl = document.getElementById('seatac');
-    // console.log(ulEl, 'ulEl');
-    for(var i = 0; i < hours.length; i++){
+// +++++++++++++++++++++++++++++++++++++++
+// EXECUTE ACTIONS (CALL FUNCTIONS)
+// +++++++++++++++++++++++++++++++++++++++
 
-      // create an element
-      var liEl = document.createElement('li');
-
-      // give it content
-      // 11am: 77 cookies
-      liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies';
-
-      // append it to the parent
-      ulEl.appendChild(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = 'Total: ' + this.totalDailySales + ' cookies';
-    ulEl.appendChild(liEl);
-  }
+makeHeaderRow();
+for(var i = 0; i < allLocations.length; i++){
+  allLocations[i].render();
 }
-
-pikePlaceMarket.render();
-seatacAirport.render();
---
+++ 08-review-debugging/lab/README.md
@@ -0,0 +1,16 @@
+# Assignment Overview: Lab 9
+
+Continue working on the functionality of your **sales.html** page if you do not have it working 100% yet, including the row of totals across the bottom.
+
+Here's your other tasks for today:
+
+- Style up the public facing page (**index.html**) to make it appealing to customers. NOTE: You **must** use all of the images in the adjacent asset directory. Your client insists that you use Every. Single. One.
+- Your public-facing **index.html** page should have all of the things you'd expect such a page to have for such a business: locations with addresses, hours open, contact information, and so us. Just do mockups of these things since this is not a real business, of course.
+- You can also mock up references (either links or little sections on the **index.html** page) for things like About Us (company history and mission), Merchandise (sales of t-shirts, mugs, stickers, etc.), links to salmon events like the Salmon Days in Issaquah, etc.
+
+- Apply a similar style to the **sales.html** page.
+
+	### Stretch Goals (Not required! Do either one, both, or neither)
+
+- Any stretch goals from prior days that you have not completed.
+- Make a style guide, using [this one that a team Brian was on made for Everlast](http://everlast.com/style-guide) as a model.
